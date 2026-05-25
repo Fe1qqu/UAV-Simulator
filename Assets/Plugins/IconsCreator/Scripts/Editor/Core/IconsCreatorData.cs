197c21b7f953d2110b65be87a97b3de16c90592b
Alex <aleksandrdorofeev04@gmail.com>
2026-05-25 15:50:33 +0500

    Added IconCreator plugin

---
+++ Assets/Plugins/IconsCreator/Scripts/Editor/Core/IconsCreatorData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using IconsCreationTool.Editor.Utility.Extensions;
+using UnityEngine;
+
+namespace IconsCreationTool.Editor.Core
+{
+    public struct IconsCreatorData
+    {
+        public int Size { get; }
+        public float Padding { get; }
+        public string Prefix { get; }
+        public string Suffix { get; }
+        public IconBackgroundData BackgroundData { get; }
+        public Texture2D FrameTexture { get; }
+        public GameObject[] Targets { get; }
+        public bool RenderShadows { get; }
+
+
+        public IconsCreatorData(int size, float padding, string prefix, string suffix,
+            IconBackgroundData backgroundData, Texture2D frameTexture, List<Object> targets, bool renderShadows)
+        {
+            Size = size;
+            Padding = padding;
+            Prefix = prefix;
+            Suffix = suffix;
+            BackgroundData = backgroundData;
+            FrameTexture = frameTexture;
+            Targets = targets.ExtractAllGameObjects().Where(g => g.HasVisibleMesh()).ToArray();
+            RenderShadows = renderShadows;
+        }
+    }
+}
